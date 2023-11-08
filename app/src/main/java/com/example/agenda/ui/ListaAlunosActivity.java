addc72e43883883401771c06edad1f8ed33f2f21
Rafael <rafa.viana.moc@gmail.com>
2023-11-08 07:27:19 -0300

    Add scrollView

--- app/src/main/java/com/example/agenda/ui/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/ListaAlunosActivity.java
@@ -35,10 +35,6 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         configuraBotao();
         configuraLista();
-
-        for (int i = 0; i < 20; i++) {
-            dao.salva(new Aluno("Rafa", "", ""));
-        }
     }
 
     @Override
