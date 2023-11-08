d6fea3565668d293b90046476afc4723788157db
Rafael <rafa.viana.moc@gmail.com>
2023-11-08 10:30:26 -0300

    INspect the project

--- app/src/main/java/com/example/agenda/model/Aluno.java
+++ app/src/main/java/com/example/agenda/model/Aluno.java
@@ -9,8 +9,7 @@ public class Aluno implements Serializable {
     private String telefone;
     private String email;
 
-    public Aluno(String nome, String telefone, String email)
-    {
+    public Aluno(String nome, String telefone, String email) {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
@@ -18,14 +17,19 @@ public class Aluno implements Serializable {
 
     @NonNull
     @Override
-    public String toString()
-    { return nome; }
+    public String toString() {
+        return nome;
+    }
 
-    public String getNome()
-    { return nome; }
+    public String getNome() {
+        return nome;
+    }
 
-    public String getTelefone()
-    { return telefone; }
+    public String getTelefone() {
+        return telefone;
+    }
 
-    public String getEmail()
-    { return email; }
+    public String getEmail() {
+        return email;
+    }
+}
